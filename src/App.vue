48c612a22c324c707793d38be75edceaa28f6392
qin <1309075777@qq.com>
2018-06-12 01:41:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <el-row :gutter="10">
+      <el-col :xs="20" :lg="4"><Nav /></el-col>
+      <el-col :xs="20" :lg="4"><router-view/></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Nav from "@/components/Nav.vue";
+export default {
+  name: 'App',
+  components: { Nav }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+    <h3>ZSQin https://www.sillycode.cn</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Silly Code. (Build by Vue.js)'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  font-weight: normal;
+}
+</style>
